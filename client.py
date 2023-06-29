0dc02a788505f4326abcc1390e78b59a72ee9f58
anhnamit03 <nam001202030355@gmail.com>
2023-06-29 21:39:31 +0700

    sf

---
+++ client.py
@@ -0,0 +1,15 @@
+# import socket
+# HOST = "127.0.0.1"
+# SERVER_PORT =65432
+# format ="utf8"
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print("Connecting to")
+# client.connect((HOST, SERVER_PORT))
+# print("client address: " , client.getsockname())
+# username = input("username: ")
+# psw = input("psw: ")
+# client.sendall(username.encode(format))
+
+# client.recv(1024)
+
+# client.sendall(psw.encode(format))
